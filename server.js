fb0aff7eb913d9168522752a430bfb61e318931f
Alison Monteiro <alisonmonteiro.10@gmail.com>
2020-02-02 21:39:59 -0300

    Use es6 and clean up

--- server.js
+++ server.js
@@ -1,50 +1,30 @@
-var express = require('express');
-var geoip = require('geoip-lite');
+const express = require('express');
+const geoip = require('geoip-lite');
 
-var port = process.env.PORT || 8000;
-var dev = process.env.NODE_ENV !== 'production';
-var app = express();
+const app = express();
+const port = process.env.PORT || 8000;
 
 // Enable CORS
-app.all('*', function(req, res, next) {
+app.all('*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   res.header('Content-Type', 'application/json');
   next();
 });
  
-app.get('/api', function(req, res) {
-  // Getting the ip of the client from the query parameter, request headers or remoteAddress
-  var ip = req.query.ip;
-  var xForwardedFor = (req.headers['x-forwarded-for'] || '').split(/,/)[0];
-  var remoteAddress = req.connection.remoteAddress;
-  var geo;
-  var response;
+app.get('/api', (req, res) => {
+  // Getting the user's IP from the query parameter;
+  const ip = req.query.ip;
+  const geo = geoip.lookup(ip);
 
-  ip = ip || xForwardedFor || remoteAddress;
+  if (!geo) {
+    return res.status(422).json({
+      ...geo,
+      error: `IP ${ip} not found.`,
+    });
+  }
 
-  geo = geoip.lookup(ip);
-  if(geo === undefined || geo === null) {
-    geo = {};
-    geo.ip = ip;
-    geo.error = 'undefined';
-  } else {
-    geo.ip = ip;
-    geo.error = false;
-  }
-  response = JSON.stringify(geo);
-  /*
-  { ip: 1.2.3.4,
-    range: [ 3479299040, 3479299071 ],
-    country: 'US',
-    region: 'CA',
-    city: 'San Francisco',
-    ll: [37.7484, -122.4156] }
-    */  
-  if (dev) {
-    console.log('reply', response);
-  }
-  res.end(response);
+  return res.status(200).json(geo);
 });
 
 app.listen(port, function() {
